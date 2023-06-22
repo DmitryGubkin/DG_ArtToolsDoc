c24fd64f775c9791c8956a7ecc1bb13471c196d2
Dmitry <stalkerportal1@gmail.com>
2023-06-22 07:40:33 +0300

    !U test video player

---
+++ docs/3dsMax/orientToWorld/license.md
@@ -0,0 +1,6 @@
+# Video example
+
+Lorem ipsum dolor sit amet
+
+![type:video](https://www.youtube.com/embed/LXb3EKWsInQ)
+  
--- installModules.py
+++ installModules.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-modulesList = ['mkdocs', 'mkdocs-material', 'mkdocs-awesome-pages-plugin']
+modulesList = ['mkdocs', 'mkdocs-material', 'mkdocs-awesome-pages-plugin', 'mkdocs-video']
 
 for m in modulesList:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', m])
